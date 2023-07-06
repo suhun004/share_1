60dbad3d1709bec5aa14674682b481316616320d
suhun004 <129172096+suhun004@users.noreply.github.com>
2023-07-06 06:53:53 +0000

    asd

--- project1.cpp
+++ project1.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
+#include <stdio.h>
+#define MAX 5
 
 using namespace std;
 
+int fun(int, int, int);
+
+int *nump;
+int arr[5] = {0, };
+
 int main() {
-  int i = 0;
+  int num = 0;
+  for (int i = 0; i < MAX; i++) {
+    
+  }
+
   
-  if (i == 0) {
+  for (int i = 0; i < MAX; i++) {
+    if (i == 0) {
+      nump = &num;
+    }
+    else {
+      nump += i;
+    }
+  }
+  if (num == NULL) {
     cout
     << "fail" << endl;
   }
@@ -15,4 +34,11 @@ int main() {
   }
 
   return 0;
+}
+
+int fun1(int a, int b, int c) {
+  if (a + b == c) {
+    return 0;
+  }
+  return 1;
 }
